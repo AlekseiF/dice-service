44dfba848e8c65d748c138115fa891edefbad0ad
Aleksei <aleksei.freidlin@gmail.com>
2019-10-16 01:37:26 +0300

    removed unused imports

--- src/test/java/com/freidlin/dice/controllers/PlayControllerTest.java
+++ src/test/java/com/freidlin/dice/controllers/PlayControllerTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
